0045adc45073c022a2e4e25bee991c736390bbf5
Hussein Dowi <56203425+husseindowi11@users.noreply.github.com>
2025-04-22 07:39:38 +0300

    Add  trait to Department model and Create  with predefined department names

---
+++ database/factories/DepartmentFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Department>
+ */
+class DepartmentFactory extends Factory
+{
+    protected static array $departments = [
+        'Human Resources',
+        'Finance',
+        'Marketing',
+        'Engineering',
+        'Sales',
+        'Customer Support',
+        'IT',
+        'Operations',
+        'Legal',
+        'Research & Development',
+    ];
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = $this->faker->unique()->randomElement(self::$departments);
+        return [
+            'name' => $name,
+        ];
+    }
+}
