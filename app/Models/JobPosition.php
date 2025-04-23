bf9d82feb1c90d3ba5afcf645df935acc8098b3c
Hussein Dowi <56203425+husseindowi11@users.noreply.github.com>
2025-04-23 06:19:05 +0300

    add job position factory and enable has factory on job position model

--- app/Models/JobPosition.php
+++ app/Models/JobPosition.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Sluggable\HasSlug;
 use Spatie\Sluggable\SlugOptions;
@@ -9,6 +10,7 @@ use Spatie\Sluggable\SlugOptions;
 class JobPosition extends Model
 {
     use HasSlug;
+    use HasFactory;
 
     protected $fillable = [
         'name',
