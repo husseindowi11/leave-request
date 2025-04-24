79026fd6e1584d970e3ae97a2d0e0b434d8bf945
Hussein Dowi <56203425+husseindowi11@users.noreply.github.com>
2025-04-24 06:49:22 +0300

    add leave request model & migration

---
+++ app/Models/LeaveRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LeaveRequest extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'leave_type_id',
+        'status',
+        'start_date',
+        'end_date',
+        'description',
+    ];
+}
